df268a81e52cf009611457392ee4851a15fb3609
AimakhanSaken <aimakhansaken90@gmail.com>
2022-06-14 14:41:54 +0600

    day 2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,1 @@
-//peremennie
-//const
-//const name = "Zigi";
-
-//let 
-//let name = "Zigi";
-
-//var 
-//var name = "Zigi";
-
-//razniva mezhdy let i var oblast vidimosti. var viden vezde a let tolko tam gde on zadeistvovan.
-//var pochti nikto yzhe ne ispolzyet.
-
-const age = prompt("skolko vam let?")
-
-alert `vam ${age} let/god`
-console.log(age)
 
